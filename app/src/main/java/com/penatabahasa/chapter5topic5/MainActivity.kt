9418a7e74304306118e1737124c4518e845f5cae
mjafarshidik <mjafarshidik86@gmail.com>
2022-04-15 10:12:38 +0700

    Initial Commit

---
+++ app/src/main/java/com/penatabahasa/chapter5topic5/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.penatabahasa.chapter5topic5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.penatabahasa.chapter5topic5.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        showName()
+        showSnackBar()
+    }
+
+    private fun showSnackBar() {
+        binding.apply {
+            val snackBar = Snackbar.make(btnSnackBar, "Bebas", Snackbar.LENGTH_INDEFINITE)
+            snackBar.setAction("back"){
+                onBackPressed()
+            }
+            snackBar.setAction("oke"){
+                Toast.makeText(this@MainActivity, "Oke cip", Toast.LENGTH_LONG).show()
+            }
+            btnSnackBar.setOnClickListener {
+                snackBar.show()
+            }
+        }
+    }
+
+    private fun showName() {
+        binding.apply {
+            btnShowName.setOnClickListener {
+                val name = edtName.text.toString()
+                Toast.makeText(this@MainActivity, name, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
